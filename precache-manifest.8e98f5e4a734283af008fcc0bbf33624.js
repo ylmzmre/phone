a6926f5c08cce8d91c8703c8ecdc9c18590db9f7
Emre Yılmaz <emreyilmaz@Emres-MacBook-Pro.local>
2020-07-10 11:55:22 +0300

    Updates

---
+++ precache-manifest.8e98f5e4a734283af008fcc0bbf33624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67de30169a4597496831c94a2b649d5a",
+    "url": "/phone/index.html"
+  },
+  {
+    "revision": "e79d82d405e0d5decbc7",
+    "url": "/phone/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b0730abab998461a41e2",
+    "url": "/phone/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e79d82d405e0d5decbc7",
+    "url": "/phone/static/js/2.ab056efd.chunk.js"
+  },
+  {
+    "revision": "17edf3bd1d923ccda4afa4de349cdf0e",
+    "url": "/phone/static/js/2.ab056efd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0730abab998461a41e2",
+    "url": "/phone/static/js/main.a53c5dd2.chunk.js"
+  },
+  {
+    "revision": "9c381aaa483479c0c6ee",
+    "url": "/phone/static/js/runtime-main.0a5130d7.js"
+  }
+]);
